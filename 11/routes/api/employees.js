6493c039a853de54b0f36ab5d992e5f8df6821e9
Hency Dsouza <hencyhd39@gmail.com>
2024-07-09 18:10:06 +0530

    JWT

---
+++ 11/routes/api/employees.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const employeesController = require('../../controllers/employeesController');
+
+const data = {}
+data.employees = require('../../model/employees.json')
+
+router.route('/')
+    .get(employeesController.getAllEmployees)
+    .post(employeesController.createNewEmployee)
+    .put(employeesController.updateEmployee)
+    .delete(employeesController.deleteEmployee)
+
+router.route('/:id')
+    .get(employeesController.getEmployee)
+
+module.exports = router
